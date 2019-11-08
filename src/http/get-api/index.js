319a4703d88464b27f75478ad926b29b6a0a4567
Ryan Block <rblock@gmail.com>
2019-11-08 14:41:44 -0800

    Add anti-caching to default response; remove unnecessary .arc-config

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,7 +1,10 @@
 exports.handler = async function http (req) {
   console.log('Begin API called')
   return {
-    headers: {'content-type': 'application/json; charset=utf8'},
+    headers: {
+      'content-type': 'application/json; charset=utf8',
+      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+    },
     body: JSON.stringify({
       message: 'Hello from your Begin API!'
     })
