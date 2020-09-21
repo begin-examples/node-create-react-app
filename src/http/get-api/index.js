06537f59b3549832e819bd1d518aa5e8be4de0cb
kj <kristoferjoseph@gmail.com>
2020-09-21 13:26:28 -0700

    Updates to sandbox 2.0. Makes compatible with AWS HTTP APIs.

--- src/http/get-api/index.js
+++ src/http/get-api/index.js
@@ -1,12 +1,6 @@
 exports.handler = async function http (req) {
   console.log('Begin API called')
   return {
-    headers: {
-      'content-type': 'application/json; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body: JSON.stringify({
-      message: 'Hello from your Begin API!'
-    })
+    message: 'Hello from your Begin API!'
   }
 }
